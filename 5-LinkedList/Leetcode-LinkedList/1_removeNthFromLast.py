927ad4851ef022c78595818cd2db5d613d84346b
Ishaan Thakur <ishaant99@gmail.com>
2022-05-14 22:19:14 -0300

    Commit

--- 5-LinkedList/Leetcode-LinkedList/1_removeNthFromLast.py
+++ 5-LinkedList/Leetcode-LinkedList/1_removeNthFromLast.py
@@ -1,28 +1,48 @@
 def removeNthFromLast(n,head):
-    length = 0
-    current = head
+    pass
+    # Solution 2 (Leetcode)
 
-    while current:
-        current = current.next
-        length += 1
-
-    if n > length:
-        return None
+    # dummy = ListNode(0,head)
+    # pointer1 = dummy
+    # pointer2 = head
         
-    current = head
-        
-    if n == length:
-        current = current.next
-        return current
-        
-    findIndex = length - n
-    index = 0
-
-    while current.next:
-        if index == findIndex-1:
-            current.next = current.next.next
-            return head
-        else:
-            index += 1
+    # for i in range(n):
+    #     pointer2 = pointer2.next
             
-        current = current.next
+    # while pointer2:
+    #     pointer1 = pointer1.next
+    #     pointer2 = pointer2.next
+            
+    # pointer1.next = pointer1.next.next
+                
+    # return dummy.next
+
+    # Solution 1
+
+    # length = 0
+    # current = head
+
+    # while current:
+    #     current = current.next
+    #     length += 1
+
+    # if n > length:
+    #     return None
+        
+    # current = head
+        
+    # if n == length:
+    #     current = current.next
+    #     return current
+        
+    # findIndex = length - n
+    # index = 0
+
+    # while current.next:
+    #     if index == findIndex-1:
+    #         current.next = current.next.next
+    #         return head
+    #     else:
+    #         index += 1
+            
+    #     current = current.next
